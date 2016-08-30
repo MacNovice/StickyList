f6cd21f6e70bdeb264d77d0bc8f605ba263e76cf
MacNovice <q1062932288@qq.com>
2016-08-30 14:34:21 +0800

    create

---
+++ listTest/src/main/java/com/example/listtest/TestMainActivity.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.example.listtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class TestMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_list_item_layout);
+	}
+
+}
